709f1684bb5e0206bd28589f5e8d67f8d630b426
fabricio <fabricio.jacob@outlook.com>
2022-04-06 21:56:11 -0300

    add

---
+++ src/main/java/com/github/lambda/Person.java
@@ -0,0 +1,15 @@
+package com.github.lambda;
+
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
---
+++ src/test/java/com/github/lambda/LambdaHandlerTestIT.java
@@ -0,0 +1,9 @@
+package com.github.lambda;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class LambdaHandlerTestIT extends LambdaHandlerTest {
+
+    // Execute the same tests but in native mode.
+}
